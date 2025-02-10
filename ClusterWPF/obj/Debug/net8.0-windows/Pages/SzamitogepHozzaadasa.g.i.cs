e7bb5875a2d57ef5158151b1be08664374dc9f72
Szidor Mihály <70961855+szidimisi68@users.noreply.github.com>
2025-02-10 10:33:40 +0100

    Update gitignore again

--- ClusterWPF/obj/Debug/net8.0-windows/Pages/SzamitogepHozzaadasa.g.i.cs
+++ ClusterWPF/obj/Debug/net8.0-windows/Pages/SzamitogepHozzaadasa.g.i.cs
@@ -85,7 +85,7 @@ namespace ClusterWPF.Pages {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -101,7 +101,7 @@ namespace ClusterWPF.Pages {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.0.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
