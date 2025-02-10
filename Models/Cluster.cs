ee1d61fd0f60b082d612c2c7153ce2cf79811819
Szidor Mihály <70961855+szidimisi68@users.noreply.github.com>
2025-02-10 11:12:05 +0100

    removed trash

--- Models/Cluster.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ConsoleApp1
-{
-    public class Cluster
-    {
-        public List<ScheduledProgram> ScheduledPrograms { get; set; } = new List<ScheduledProgram>();
-        public List<Instance> Instances { get; set; } = new List<Instance>();
-    }
-}
