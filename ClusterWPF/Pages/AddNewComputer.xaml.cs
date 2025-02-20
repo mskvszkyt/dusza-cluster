1ecac49a8f9207aea99658c5e59699a6379c9f73
mskvszkyt <muskovszkyt@gmail.com>
2025-02-20 13:09:33 +0100

    addnewcomputer translated

--- ClusterWPF/Pages/AddNewComputer.xaml.cs
+++ ClusterWPF/Pages/AddNewComputer.xaml.cs
@@ -1,28 +1,16 @@
 ﻿/// <summary>
-/// Interaction logic for SzamitogepHozzaadasa.xaml
+/// Interakciós logika a SzamitogepHozzaadasa.xaml számára
 /// </summary>using ConsoleApp1;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using ConsoleApp1;
 using Path = System.IO.Path;
 
 namespace ClusterWPF.Pages
 {
     /// <summary>
-    /// Interaction logic for SzamitogepHozzaadasa.xaml
+    /// Interakciós logika a SzamitogepHozzaadasa.xaml számára
     /// </summary>
     public partial class AddNewComputer : Page
     {
@@ -56,17 +44,15 @@ namespace ClusterWPF.Pages
 
                 _cluster.Instances.Add(newInstance);
 
-
                 string instancePath = Path.Combine(_path, instanceName);
                 Directory.CreateDirectory(instancePath);
                 File.WriteAllText(Path.Combine(instancePath, ".szamitogep_config"), $"{processor}\n{memory}");
 
-                MessageBox.Show("New computer added successfully!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
-
+                MessageBox.Show("Új számítógép sikeresen hozzáadva!", "Sikeres", MessageBoxButton.OK, MessageBoxImage.Information);
             }
             else
             {
-                MessageBox.Show("Please enter valid numeric values for processor and memory.", "Invalid Input", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("Kérjük, érvényes numerikus értékeket adjon meg a processzorhoz és a memóriához.", "Érvénytelen bevitel", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
     }
